2cc513111defbe8b9d4221edf997193705483917
riski putra <subkhanpinter22@gmail.com>
2022-02-23 11:34:31 +0700

    new2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello world",
+      title: "Hello world2",
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.stars),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
